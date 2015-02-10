7fc7ba1a97ac15684bd326ed99ba9d8a045538cb
mnuthai <mnuthai@gmail.com>
2015-02-09 18:57:23 -0800

    changed database testing

--- ping_db.sh
+++ ping_db.sh
@@ -8,16 +8,29 @@ then
 fi
 
 if [ "$DATABASE_TYPE" = "MongoDB" ]
-	mongo --host "$DATABASE_HOST" --eval "db.stats()"
-	res="$?"
+then
+  mongo --host "$DATABASE_HOST" --c "db.testData.insert( { x : 1 } )"
+  res1="$?"
+  mongo --host "$DATABASE_HOST" --c "db.testData.find()"
+  res2="$?"
+  mongo --host "$DATABASE_HOST" --c "db.testData.remove()"
+  res3="$?"
 elif [ "$DATABASE_TYPE" = "PostgreSQL" ]
 then	
 	service postgresql status
 	res="$?"
 fi
 
-if [ "$res" -ne 0 ] 
+if [ "$res1" -ne 0 ] 
 then
-	echo "Unable to query $DATABASE_TYPE at $DATABASE_HOST"
-	exit "$res"
+	echo "Unable to insert data in $DATABASE_TYPE at $DATABASE_HOST"
+	exit "$res1"
+elif [ "$res2" -ne 0 ]
+then
+	echo "Unable to query data in $DATABASE_TYPE at $DATABASE_HOST"
+	exit "$res2"
+elif [ "$res3" -ne 0 ]
+then
+	echo "Unable to delete data in $DATABASE_TYPE at $DATABASE_HOST"
+	exit "$res3"
 fi 
